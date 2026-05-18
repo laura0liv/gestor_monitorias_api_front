e287bcb6370d3fb2964e0fd594bf82a8e3f7df65
laura0liv <laura.olivella.macias@gmail.com>
2026-05-18 18:58:16 -0500

    cambios en tutor y home page

--- src/routes/Tutor/+page.svelte
+++ src/routes/Tutor/+page.svelte
@@ -2,11 +2,11 @@
   import { onMount } from "svelte";
   import { usuario } from '$lib/stores/user';
   import { goto } from '$app/navigation';
-  import Disponibilidad       from "$lib/components/Tutor/Disponibilidad.svelte";
-  import MisMateriasAsignadas from "$lib/components/Tutor/MisMateriasAsignadas.svelte";
-  import SesionesAgendadas    from "$lib/components/Tutor/SesionesAgendadas.svelte";
-  import RegistrarResultado   from "$lib/components/Tutor/RegistrarResultado.svelte";
-  import HistorialMonicorias  from "$lib/components/Tutor/HistorialMonitorias.svelte";
+  import Disponibilidad       from "$lib/components/tutor/Disponibilidad.svelte";
+  import MisMateriasAsignadas from "$lib/components/tutor/MisMateriasAsignadas.svelte";
+  import SesionesAgendadas    from "$lib/components/tutor/SesionesAgendadas.svelte";
+  import RegistrarResultado   from "$lib/components/tutor/RegistrarResultado.svelte";
+  import HistorialMonicorias  from "$lib/components/tutor/HistorialMonitorias.svelte";
 
   let modulo = $state("disponibilidad");
   let user = $state(null);
