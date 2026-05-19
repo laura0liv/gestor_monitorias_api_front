20dc5eabf3d038d4af55dfb4b49cdf2b7653d9b0
laura0liv <laura.olivella.macias@gmail.com>
2026-05-18 19:18:09 -0500

    Renombra carpetas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script>
   import Hero from '$lib/components/HomePage/Hero.svelte';
   import Features from '$lib/components/HomePage/Features.svelte';
-   import Rolessection from '$lib/components/HomePage/Rolessection.svelte';
-  import Processsection from '$lib/components/HomePage/Processsection.svelte';
+   import RolesSection from '$lib/components/HomePage/RolesSection.svelte';
+  import ProcessSection from '$lib/components/HomePage/ProcessSection.svelte';
   import Footer from '$lib/components/HomePage/Footer.svelte';
 </script>
 
 <div class="homepage">
   <Hero />
   <Features />
-  <Rolessection />
-  <Processsection />
+  <RolesSection />
+  <ProcessSection />
   <Footer />
 </div>
 
